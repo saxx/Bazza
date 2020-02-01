b97b610187096cf506fb9daba1a7bdcec053b9e1
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2020-02-01 18:28:00 +0100

    Add row count when adding articles.

--- src/Bazza/wwwroot/js/site.js
+++ src/Bazza/wwwroot/js/site.js
@@ -13,12 +13,14 @@ addButton.on("click", function() {
    var newRow = templateRow.clone();
    newRow.prop("id", "");
    newRow.css("display", "table-row");
-   newRow.find("td:nth-of-type(1) input").prop("name", "Articles[" + (currentCount - 1) + "].Name");
-   newRow.find("td:nth-of-type(2) input").prop("name", "Articles[" + (currentCount - 1) + "].Size");
-   newRow.find("td:nth-of-type(3) input").prop("name", "Articles[" + (currentCount - 1) + "].Price");
+   
+   newRow.find("td:nth-of-type(1)").text(currentCount);
+   newRow.find("td:nth-of-type(2) input").prop("name", "Articles[" + (currentCount - 1) + "].Name");
+   newRow.find("td:nth-of-type(3) input").prop("name", "Articles[" + (currentCount - 1) + "].Size");
+   newRow.find("td:nth-of-type(4) input").prop("name", "Articles[" + (currentCount - 1) + "].Price");
    
    $("table tbody").append(newRow);
-   newRow.find("td:nth-of-type(1) input").focus();
+   newRow.find("td:nth-of-type(2) input").focus();
 });
 
 removeButton.on("click", function() {
