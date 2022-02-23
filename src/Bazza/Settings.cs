1382c8856e02260b697c9a72537f3c224a1be0ce
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2022-02-23 13:22:58 +0100

    Close registration for today.

--- src/Bazza/Settings.cs
+++ src/Bazza/Settings.cs
@@ -7,7 +7,7 @@ public static class Settings
     public static DateTime StartDate = new DateTime(2022, 03, 04).Date;
     public static DateTime EndDate = new DateTime(2022, 03, 05).Date;
     public static DateTime OnlineStartDate = new DateTime(2022, 2, 14);
-    public static DateTime OnlineEndDate = new DateTime(2022, 2, 28);
+    public static DateTime OnlineEndDate = new DateTime(2022, 2, 22);
     public static string MaxNumberOfArticles = "6.000";
     public static DateTime BringArticlesDate = new DateTime(2022, 03, 3).Date;
 }
