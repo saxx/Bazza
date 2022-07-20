55fd61e2130490f20f7debd82fbdd594c67e6d1d
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2022-07-20 17:12:52 +0200

    Update texts and dates for Sep-22 basar.

--- src/Bazza/Settings.cs
+++ src/Bazza/Settings.cs
@@ -4,10 +4,10 @@ namespace Bazza;
 
 public static class Settings
 {
-    public static DateTime StartDate = new DateTime(2022, 03, 04).Date;
-    public static DateTime EndDate = new DateTime(2022, 03, 05).Date;
-    public static DateTime OnlineStartDate = new DateTime(2022, 2, 14);
-    public static DateTime OnlineEndDate = new DateTime(2022, 2, 22);
-    public static string MaxNumberOfArticles = "6.000";
-    public static DateTime BringArticlesDate = new DateTime(2022, 03, 3).Date;
+    public static DateTime StartDate = new(2022, 09, 09);
+    public static DateTime EndDate = new(2022, 09, 10);
+    public static DateTime OnlineStartDate = new(2022, 08, 23);
+    public static DateTime OnlineEndDate = new(2022, 09, 05);
+    public static int MaxNumberOfArticles = 6000;
+    public static DateTime BringArticlesDate = new DateTime(2022, 09, 08).Date;
 }
